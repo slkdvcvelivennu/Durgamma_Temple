516ffab44f9e889b427b7faaaf6dd811f2f8249e
slkdvcvelivennu <slkdvcvelivennu@gmail.com>
2026-09-25 21:24:57 +0530

    Use Apps Script path-based API endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec?api=transactions",
-  ADMIN_APP_URL: "https://script.google.com/macros/s/AKfycbyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec?page=admin"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfcybyobUwIKaScnHUOWqz8nEqD8u0DzFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec",
+  ADMIN_APP_URL: "https://script.google.com/macros/s/AKfcybyobUwIKaScnHUOWqz8nEqD8u0DzFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec"
 };
