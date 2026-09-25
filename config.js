fbd51a1e741ee4593ffbd010d26fb66eaeefa2a7
slkdvcvelivennu <slkdvcvelivennu@gmail.com>
2026-09-25 18:59:23 +0530

    Fix public API endpoint and keep separate admin portal

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec?api=transactions",
   ADMIN_APP_URL: "https://script.google.com/macros/s/AKfycbyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec?page=admin"
 };
