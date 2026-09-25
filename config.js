19ff75eac64ff0ce64381c77f6588822f52fe251
slkdvcvelivennu <slkdvcvelivennu@gmail.com>
2026-09-25 22:32:43 +0530

    Point Admin button at the newly deployed admin web app
    The old ADMIN_APP_URL returned 404 (script no longer exists). Replace
    with the new deployment and bump the script cache-buster.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwm65WKlH1fokxSy-F1EkAVWMuOkDlzBo4xTDi2GplarnsWuv8ENgcWnvor5gntgAnr/exec",
-  ADMIN_APP_URL: "https://script.google.com/macros/s/AKfcybyobUwIKaScnHUOWqz8nEqD8u0DzSjFnf39dtu97AAd3Z9FjnPiBxWTlKDi6OKfPej1/exec"
+  ADMIN_APP_URL: "https://script.google.com/macros/s/AKfycbzsQrfs2SCh6d3SGngIOByQZCU5SuB5gWX-hH2UQy-dRRG8r0OjNSO5jrDRODL4nTSQ/exec"
 };
